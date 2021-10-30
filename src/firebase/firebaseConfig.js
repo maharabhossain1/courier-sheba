349ca7619a6b7bf62328321c7ebe088eff743788
maharabhossain1 <maharabhossain0022@gmail.com>
2021-10-30 14:53:41 +0600

    deplyed

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDDP2pLQq60uQC1Cpx0-PSjycP1OInuCBg",
-  authDomain: "courier-service-2021.firebaseapp.com",
-  projectId: "courier-service-2021",
-  storageBucket: "courier-service-2021.appspot.com",
-  messagingSenderId: "406032691621",
-  appId: "1:406032691621:web:45fa3ad7d6ad05940c384c",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
